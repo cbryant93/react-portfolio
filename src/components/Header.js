2578b00bbcfd75875e7019a77094ab2da40f17db
cbryant93 <cbryant1993@gmail.com>
2023-03-13 23:31:55 +0000

    Added SCSS to navbar - aligned it to top of screen and made it responsive for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,28 @@
-import  React from "react";
+import  React, { useState } from "react";
 import "./styles/Header.scss"
-import { Close } from "@material-ui/icons";
+import { Close, MenuOutlined } from "@material-ui/icons";
 import {A} from 'hookrouter'; 
 
 
 const Header = () => {
-    return(
-<div>
-    <header className="header">
 
-        <div className="header_logo">
+     const [active, setActive] = useState(false);
+
+     const showMenu =() =>{
+        setActive(!active)
+     }
+
+    return(
+    <div className="header">
+
+        <div className="header__logo">
             <h1>Christian Bryant</h1>
         </div>
 
-        <nav>
+        <nav className="navbar">
             <ul>
         <div className= "closed">
-            <Close className = 'close'/>
+            <Close className = 'close' onClick ={showMenu}/>
         </div>
             <li>
             <A className='Link' href='/'>HOME</A> 
@@ -31,7 +37,7 @@ const Header = () => {
             <A className='Link' href='/'>PROJECTS</A> 
             </li>
             <li>
-            <A className='Link' href='/'>OTHER</A> 
+            <A className='Link' href='/'>BLOG</A> 
             </li>
             <li>
             <A className='Link' href='/'>CONTACT ME</A> 
@@ -39,9 +45,12 @@ const Header = () => {
             </ul>
         </nav>
 
-    </header>
+        <div className="changer">
+            <MenuOutlined className="menu" onClick ={showMenu}/>
+        </div>
 
-</div>
+    </div>
+
     )
 }
 
