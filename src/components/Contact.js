cca2bf67825ac37f96b44e5575bdef78798990ef
cbryant93 <cbryant1993@gmail.com>
2023-04-11 23:10:01 +0100

    Trying to get success messsage to work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Contact.scss";
 
 function Contact() {
+  const [messageSent, setMessageSent] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    const form = event.target;
+    const formData = new FormData(form); // Get form data
+
+    // Send form data to server using AJAX
+    fetch('http://localhost/api/contact/index.php', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Display success message to user
+        setMessageSent(true);
+
+        // Clear form fields
+        form.reset();
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
-    <div class="contact" id="Contact">
+    <div className="contact" id="Contact">
       <div className="inputs">
-      <h1>CONTACT ME</h1>
-        <form action="http://localhost/api/contact/index.php" method="POST" class="form">
+        <h1>CONTACT ME</h1>
+        <form onSubmit={handleSubmit} className="form">
           <label htmlFor="name">Your Name</label>
           <input
             type="text"
             id="name"
             name="name"
             placeholder="Enter Name..."
-            tabindex="1"
+            tabIndex="1"
             required
           />
 
@@ -23,7 +47,7 @@ function Contact() {
             id="email"
             name="email"
             placeholder="example@123.com"
-            tabindex="2"
+            tabIndex="2"
             required
           />
 
@@ -33,7 +57,7 @@ function Contact() {
             id="subject"
             name="subject"
             placeholder="Hello There!"
-            tabindex="3"
+            tabIndex="3"
             required
           />
 
@@ -45,12 +69,15 @@ function Contact() {
             className="message"
             name="message"
             placeholder="Enter Message..."
-            tabindex="4"
+            tabIndex="4"
           ></textarea>
 
           <button type="submit" className="btn">
             Send Message!
           </button>
+          {messageSent && (
+            <p className="success">Your message has been sent!</p>
+          )}
         </form>
       </div>
     </div>
