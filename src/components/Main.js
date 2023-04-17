7fc345612a496b5c1fac9e162cf7f0577e55f901
cbryant93 <cbryant1993@gmail.com>
2023-04-17 22:54:31 +0100

    Testing out new CSS stylings

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
-import { GitHub, LinkedIn } from "@material-ui/icons";
+import { Email, GitHub, LinkedIn } from "@material-ui/icons";
 import React from "react";
 import './styles/Main.scss'
+import { Link, animateScroll as scroll } from "react-scroll";
 import me from './assests/me.jpg'
 
 const Main =() => {
@@ -17,16 +18,24 @@ const Main =() => {
                         <h1>I am Christian</h1>
                         <p>I am a SDET Engineer</p>
 
-                        <div className = "icons">
-                            <LinkedIn className="icon"/>
-                            <GitHub className="icon"/>
+                        <div className="icons">
+                            <a href="https://www.linkedin.com/in/christian-bryant/">
+                                <LinkedIn className="icon"/>
+                            </a>
+                            <a href="https://github.com/cbryant93">
+                                <GitHub className="icon"/>
+                            </a>
+                            <Link
+                            to="Contact"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            >
+                            <Email className="icon"/>
+                            </Link>
                         </div>
 
-                        <div className="buttons">
-                            <button>Contact Me</button>
-                            <button>Hire Me</button>
-
-                        </div>
                    
                     </div>
                 
