867c5f0f6e6c5a87bd8a106b396d08252563f05b
cbryant93 <cbryant1993@gmail.com>
2023-04-27 16:33:39 +0100

    Completed SCSS for the game display

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,55 +16,55 @@ function Game() {
 
   <div className="container">
 
-    <div className='gameStats'>
-        
-        <div className="cScore">
-            <h3>Carnivore Score</h3>
+        <div className='gameStats'>
+            
+            <div className="cScore">
+                <h3>Carnivore Score</h3>
+                <p id="points">0</p>
+                {/* Carnivore score is appended here */}
+            </div>
+
+            <div className="timer">
+                <h3>Timer: </h3>
+                <p id="timer"></p>
+                {/* Timer appended here */}
+            </div>
+
+            <div className="vScore">
+            <h3>Vegan Score</h3>
             <p id="points">0</p>
-            {/* Carnivore score is appended here */}
+            {/* Vegan score appended here */}
+            </div>
         </div>
 
-        <div className="timer">
-            <h3>Timer: </h3>
-            <p id="timer"></p>
-            {/* Timer appended here */}
+        <div className='gameButtons'>
+            <button className="btn" id="startButton">Start</button>
+            {/* Start Button */}
+            <button className="btn" id="how" type="button" name="button">How to play?</button>
+            {/* How to play button */}
         </div>
 
-        <div className="vScore">
-        <h3>Vegan Score</h3>
-        <p id="points">0</p>
-        {/* Vegan score appended here */}
-        </div>
-    </div>
+            <div className="gameover" id="restart">
+            <h2>GAME OVER!</h2>
+            <p id="finalScore"></p>
+            {/* Final score is appended here */}
+            <p id="endMessage"></p>
+            {/* Appropriate end message appended here */}
+            <button id="bResart" className="btn" type="button" name="button">Restart</button>
+            {/* Restart Button */}
+            </div>
 
-    <div className='gameButtons'>
-        <button className="btn" id="startButton">Start</button>
-        {/* Start Button */}
-        <button className="btn" id="how" type="button" name="button">How to play?</button>
-        {/* How to play button */}
-    </div>
-
-        <div className="gameover" id="restart">
-        <h3>GAME OVER!</h3>
-        <p id="finalScore"></p>
-        {/* Final score is appended here */}
-        <p id="endMessage"></p>
-        {/* Appropriate end message appended here */}
-        <button id="bResart" className="btn" type="button" name="button">Restart</button>
-        {/* Restart Button */}
-        </div>
-
-        <div className="instructions" id="instruct">
-        {/* Instructions message */}
-        <h3>How to play?</h3>
-        <p>Do you believe meat is murder?</p>
-        <p>Are you fed up of vegans pushing their agenda on you?</p>
-        <p>Are you neither here nor there?</p>
-        <p>Play the game how you like.</p>
-        <p>Targets will fly across the screen, try your best to click and shoot whatever reflects your beliefs.</p>
-        <button type="button" name="button" id="back">back</button>
-        {/* Back button */}
-        </div>
+            <div className="instructions" id="instruct">
+            {/* Instructions message */}
+            <h2>How to play?</h2>
+            <p>Do you believe meat is murder?</p>
+            <p>Are you fed up of vegans pushing their agenda on you?</p>
+            <p>Are you neither here nor there?</p>
+            <p>Play the game how you like.</p>
+            <p>Targets will fly across the screen, try your best to click and shoot whatever reflects your beliefs.</p>
+            <button type="button" name="button" id="back">back</button>
+            {/* Back button */}
+            </div>
     </div>    
 </div>
   );
